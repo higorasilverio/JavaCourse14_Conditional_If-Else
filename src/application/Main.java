62bc73d614dcc72638e4bba1361fbc8cf51baa55
higorasilverio <higorasilverio@outlook.com>
2020-04-10 15:10:30 -0300

    Income tax - Final

---
+++ src/application/Main.java
@@ -0,0 +1,36 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("*** Income tax ***");
+		System.out.println();
+		
+		System.out.print("Enter the income: ");
+		double income = scanner.nextDouble();
+		System.out.println();
+		if (income > 2000.00 && income <= 3000.00)	{
+			income = income * 0.08;
+			System.out.printf("U$ %.2f", income);
+		}
+		else if (income > 3000.00 && income <= 4500.00) {
+			income = (1000 * 0.08) + ((income - 3000) * 0.18);
+			System.out.printf("U$ %.2f", income);
+		}
+		else if (income > 4500.00) {
+			income = (1000 * 0.08) + (1500 * 0.18) + ((income - 4500) * 0.28);
+			System.out.printf("U$ %.2f", income);
+		}
+		else System.out.println("Tax free!");
+		
+		scanner.close();
+	}
+
+}
